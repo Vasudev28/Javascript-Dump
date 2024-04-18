551fc5e525808869b75409e6855c32bc6cbff127
Vasudev28 <120118111+Vasudev28@users.noreply.github.com>
2024-04-18 00:52:08 +0000

    now used another way to insert an element(created a new array and inserted required value to this empty new array at a specified index and then copied original array values to the new array).

--- JS_TUTS/inseringanelementintoarray.js
+++ JS_TUTS/inseringanelementintoarray.js
@@ -11,5 +11,23 @@ var a=[1,2,3,4,5,7,8,9,10];
 //  return a;
 // }
 // console.log(inser());
-a.splice(5,0,6);
-console.log(a);
+// a.splice(5,0,6);
+// console.log(a);
+var a = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+
+function inser() {
+    var newA = []; // Create a new array to store the modified elements
+    var indexTo = 5; // Index where you want to insert the new element
+    var valTo = 6; // New element to be inserted
+
+    for (var i = 0; i < a.length; i++) {
+        if (i === indexTo) {
+            newA.push(valTo); // Insert the new element at the specified index
+        }
+        newA.push(a[i]); // Copy elements from the original array to the new array
+        
+    }
+    return newA;
+}
+
+console.log(inser()); 
