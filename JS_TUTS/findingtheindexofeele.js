5883ddea39f2a358d4519998e4d071df47de8b9d
Vasudev28 <vasudevganti2879@gmail.com>
2024-05-04 12:24:44 +0530

    changes done

---
+++ JS_TUTS/findingtheindexofeele.js
@@ -0,0 +1,24 @@
+console .log("FINDING OUT THE INDEX OF 4 USING METHOD:");
+var a=[1,3,5,7,4,11,13];
+console.log(`index of 4 is ${a.indexOf(4)}`);
+console .log("FINDING OUT THE INDEX OF 4 WITHOUT USING METHOD:");
+for(i=0;i<a.length;i++){
+  if(a[i]===4){
+    console.log(`index of 4 is ${i}`);
+  }
+}
+
+console .log("FINDING OUT WHETHER 2 IS PRESENT IN THE ARRAY USING METHOD:");
+
+console.log(`is 2 present in array: ${a.includes(2)}`);
+
+console .log("FINDING OUT WHETHER 2 IS PRESENT IN THE ARRAY WITHOUT USING METHOD:");
+
+for(j=0;j<a.length;j++){
+
+}if(a[i]===2){
+  console.log(`is 2 array is in the array:${a.includes(2)}`);
+}
+else{
+   console.log(`is 2 array is in the array:${a.includes(2)}`)
+}
