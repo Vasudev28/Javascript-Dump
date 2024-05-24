e3d6e27b38509d3133fa12d9f7334dff5a5b06a8
Vasudev28 <vasudevganti2879@gmail.com>
2024-05-24 07:58:44 +0530

    changes made in the loop part

--- JS_TUTS/pssheeteasy_33.js
+++ JS_TUTS/pssheeteasy_33.js
@@ -1,14 +1,13 @@
-var c=0;
 function fun(v){
+var c=0;
 for(i=0;i<v.length;i++){
     for(j=i+1;j<v.length;j++){
-
         if(v[i]==v[j]){
+            c++;
             
-            console.log(`${v[i]} repeated`)
         }
     }
-
 }
+console.log(`${v[j]} repeated ${c} times`);
 return;
-}fun([1,3,5,6,7,8,9,0,9]);
+}fun([1,3,5,6,7,8,9,0,9,4,9]);
