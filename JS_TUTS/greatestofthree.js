87b894488589a71da7c6f5f8796eae2057150721
Vasudev28 <vasudevganti2879@gmail.com>
2024-05-27 09:21:49 +0530

    greatest of three numbers

---
+++ JS_TUTS/greatestofthree.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+var c=30;
+if(a>b && a>c){
+  console.log(" a is greater");
+}
+else if(b>a && b>c){
+   console.log(" b is greater");
+}
+else{
+  console.log(" c is greater");
+}
